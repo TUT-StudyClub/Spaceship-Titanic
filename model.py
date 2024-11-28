6e0e0a9e7fdf6e82c676cfd2e8ef0c6cdfa8dbea
c0b22175e4 <c0b22175e4@edu.teu.ac.jp>
2024-11-28 15:09:27 +0900

    修正

--- model.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-train1 = pd.read_csv('/Users/yamadayuuhei/Github/space_titanic/train.csv')
-test1 = pd.read_csv('/Users/yamadayuuhei/Github/space_titanic/test.csv')
-train2 = pd.read_csv('')
